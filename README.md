b7e82dca23ff4c57d006cc3b47c90908dcdd3e94
TanioMac <taniomac@MacBook-Pro-de-TanioMac.local>
2019-03-24 20:56:36 -0300

    .

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-rest_movies
+REST MOVIES
 
+Web Service para retornar os filmes que estão em cartaz. O retorno é em JSON
 
-Nada a declarar
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
-from urllib.request import urlopen, Request
+import requests
 import json
 import os
 
@@ -11,20 +11,18 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route('/api/filmes', methods=['GET'])
 def filmes():
-    html_doc = urlopen("http://www.adorocinema.com/filmes/numero-cinemas/").read()
-    soup = BeautifulSoup(html_doc, "html.parser")
-
+    html_doc = requests.get("http://www.adorocinema.com/filmes/numero-cinemas/")
+    soup = BeautifulSoup(html_doc.text, "html.parser")
     data = []
     for dataBox in soup.find_all("li",class_="mdl"):
         nomeObj = dataBox.find("h2", class_="meta-title").find("a", class_="meta-title-link")
-        imgObj = dataBox.find("figure", class_="thumbnail ").find("img", class_="thumbnail-img")
+        imgObj = dataBox.find("img", class_="thumbnail-img")
         sinopseObj = dataBox.find("div", class_="synopsis").find("div", class_="content-txt")
         dataObj = dataBox.find("div", class_="meta-body-item meta-body-info").find("span", class_="date")
         if imgObj.has_attr("data-src"):
             imagem = imgObj["data-src"]
         else:
             imagem = imgObj["src"]
-
         data.append({ 'nome': nomeObj.text.strip(),
                     'poster' : imagem,
                     'sinopse' : sinopseObj.text.strip(),
