ae6c004130d9b8e1bf5eec136be0642ab8d52c6f
tnioa <hortanio@hotmail.com>
2019-04-04 17:25:15 -0300

    .

--- app.py
+++ app.py
@@ -14,25 +14,30 @@ def filmes():
     html_doc = requests.get("http://www.adorocinema.com/filmes/numero-cinemas/")
     soup = BeautifulSoup(html_doc.text, "html.parser")
     data = []
-    for dataBox in soup.find_all("li",class_="mdl"):
-        nomeObj = dataBox.find("h2", class_="meta-title").find("a", class_="meta-title-link")
-        imgObj = dataBox.find("img", class_="thumbnail-img")
-        sinopseObj = dataBox.find("div", class_="synopsis").find("div", class_="content-txt")
-        dataObj = dataBox.find("div", class_="meta-body-item meta-body-info").find("span", class_="date")
-        if imgObj.has_attr("data-src"):
-            imagem = imgObj["data-src"]
-        else:
-            imagem = imgObj["src"]
-        data.append({ 'nome': nomeObj.text.strip(),
-                    'poster' : imagem,
-                    'sinopse' : sinopseObj.text.strip(),
-                    'data' :  dataObj.text.strip()})
+    for dataBox in soup.find_all("li", class_="mdl"):
 
+        if dataBox.find('div', class_='card entity-card entity-card-list cf'):
+            nomeObj = dataBox.find("h2", class_="meta-title").find("a", class_="meta-title-link")
+            imgObj = dataBox.find("figure").find("img", class_="thumbnail-img")
+            sinopseObj = dataBox.find("div", class_="synopsis").find("div", class_="content-txt")
+            dataObj = dataBox.find("div", class_="meta-body-item meta-body-info").find("span", class_="date")
+            if imgObj.has_attr("data-src"):
+                imagem = imgObj["data-src"]
+            else:
+                imagem = imgObj["src"]
+                
+            data.append({ 'nome' : nomeObj.text.strip(),
+                        'poster' : imagem,
+                        'sinopse' : sinopseObj.text.strip(),
+                        'data' :  dataObj.text.strip()})
+
+        else:
+            pass
     
     return jsonify({'filmes': data})
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
 
 
 
