8c3bef4e3aac1f723f87cb696a4eb88f3d2fc5ef
tnioa <hortanio@hotmail.com>
2019-10-02 14:21:31 -0300

    attcoors

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def filmes():
     return jsonify({'filmes': data})
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 
