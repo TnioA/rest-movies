a14418e21bed0764596859ca8ee3d922da8bc7db
tnioa <hortanio@hotmail.com>
2019-10-02 14:19:08 -0300

    attcors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 import json
@@ -38,7 +39,7 @@ def filmes():
     return jsonify({'filmes': data})
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
 
 
 
